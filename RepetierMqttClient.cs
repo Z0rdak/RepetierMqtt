37e280e0437905a6aad73c3b27371bcfd863d39f
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-02-03 14:47:49 +0100

    some cleanup

--- RepetierMqttClient.cs
+++ RepetierMqttClient.cs
@@ -27,7 +27,6 @@ namespace RepetierSharp.RepetierMqtt
         /// </summary>
         private Dictionary<string, MqttTopicFilter> CommandResponseTopics { get; set; } = new Dictionary<string, MqttTopicFilter>();
 
-
         public RepetierConnection RepetierConnection { get; private set; }
 
         public string BaseTopic { get; set; }
@@ -38,7 +37,6 @@ namespace RepetierSharp.RepetierMqtt
 
         private uint ReconnectDelay { get; set; } = 3000;
 
-        // TODO: 
         /// <summary>
         /// Topic -> Command to execute 
         /// </summary>
