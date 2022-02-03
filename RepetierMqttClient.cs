c55876055d5afc83d1bfa882cb0e1195053b2c49
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-02-03 13:49:11 +0100

    implementing most default handlers

--- RepetierMqttClient.cs
+++ RepetierMqttClient.cs
@@ -13,184 +13,23 @@ using System.Threading.Tasks;
 
 namespace RepetierSharp.RepetierMqtt
 {
-    public class RepetierMqttClient
+    public partial class RepetierMqttClient
     {
-
-        public class RepetierMqttClientBuilder
-        {
-
-            private RepetierMqttClient _repetierMqttClient = new RepetierMqttClient();
-
-            public RepetierMqttClientBuilder()
-            {
-
-            }
-
-            public RepetierMqttClientBuilder WithRepetierConnection(RepetierConnection repetierConnection)
-            {
-                _repetierMqttClient.RepetierConnection = repetierConnection;
-                return this;
-            }
-
-            public RepetierMqttClientBuilder WithMqttClientOptions(IMqttClientOptions options)
-            {
-                _repetierMqttClient.MqttClientOptions = options;
-                return this;
-            }
-
-            public RepetierMqttClientBuilder WithBaseTopic(string baseTopic)
-            {
-                _repetierMqttClient.BaseTopic = baseTopic;
-                // TODO: remove trailing /
-                return this;
-            }
-
-            public RepetierMqttClientBuilder TopicConfiguration(Tuple<string, string> commandTopicTuple)
-            {
-                _repetierMqttClient.TopicsForEvents.Add(commandTopicTuple.Item1, commandTopicTuple.Item2);
-                return this;
-            }
-
-            public RepetierMqttClientBuilder TopicConfiguration(string command, string topic)
-            {
-                _repetierMqttClient.TopicsForEvents.Add(command, topic);
-                return this;
-            }
-
-            public RepetierMqttClientBuilder WithSubscriptions(List<string> topics)
-            {
-                _repetierMqttClient.Topics.AddRange(topics);
-                return this;
-            }
-
-            public RepetierMqttClientBuilder WithSubscriptions(List<MqttTopicFilter> topics)
-            {
-                _repetierMqttClient.Subscriptions.AddRange(topics);
-                return this;
-            }
-
-            public RepetierMqttClientBuilder WithSubscription(string topic, string command)
-            {
-                _repetierMqttClient.TopicActions.Add(topic, command);
-                return this;
-            }
-
-            public RepetierMqttClientBuilder WithReconnectDelay(uint delayInMs = 3000)
-            {
-                _repetierMqttClient.ReconnectDelay = delayInMs;
-                return this;
-            }
-
-            public RepetierMqttClientBuilder DefaultQoSLevel(uint qos = 0)
-            {
-                _repetierMqttClient.DefaultQoS = (MqttQualityOfServiceLevel) qos;
-                return this;
-            }
-
-            public RepetierMqttClientBuilder DefaultQoSLevel(MqttQualityOfServiceLevel qos = MqttQualityOfServiceLevel.AtMostOnce)
-            {
-                _repetierMqttClient.DefaultQoS = qos;
-                return this;
-            }
-
-
-
-            public RepetierMqttClient Build()
-            {
-                if (_repetierMqttClient.RepetierConnection == null)
-                {
-                    throw new ArgumentNullException("RepetierConnection needs to be provided");
-                }
-
-                if (_repetierMqttClient.MqttClientOptions == null)
-                {
-                    _repetierMqttClient.MqttClientOptions = MqttOptionsProvider.DefaultMqttClientOptions;
-                }
-
-                // create subscriptions
-                if (!string.IsNullOrEmpty(_repetierMqttClient.BaseTopic))
-                {
-                    _repetierMqttClient.Topics.ForEach(topic =>
-                    {
-                        var topicFilter = new MqttTopicFilterBuilder()
-                        .WithQualityOfServiceLevel(_repetierMqttClient.DefaultQoS)
-                        .WithTopic(topic)
-                        .Build();
-                        _repetierMqttClient.Subscriptions.Add(topicFilter);
-                    });
-                    _repetierMqttClient.Subscriptions.ForEach(topicFilter => topicFilter.Topic = $"{_repetierMqttClient.BaseTopic}/{topicFilter.Topic}");
-                }
-
-                _repetierMqttClient.MqttClient = new MqttFactory().CreateMqttClient();
-
-                _repetierMqttClient.MqttClient.UseConnectedHandler(async connectedArgs =>
-                {
-                    await _repetierMqttClient.MqttClient.SubscribeAsync(_repetierMqttClient.Subscriptions.ToArray());
-                });
-
-                _repetierMqttClient.MqttClient.UseDisconnectedHandler(async disconnectedArgs =>
-                {
-                    await Task.Delay(TimeSpan.FromMilliseconds(_repetierMqttClient.ReconnectDelay));
-                    try
-                    {
-                        await _repetierMqttClient.MqttClient.ConnectAsync(_repetierMqttClient.MqttClientOptions, CancellationToken.None);
-                    }
-                    catch (Exception e)
-                    {
-                        Console.Error.WriteLine(e);
-                    }
-                });
-
-                _repetierMqttClient.RepetierConnection.OnResponse += async (callbackID, command, response) =>
-                {
-                    // TODO: check mapping and publish response
-                    if (_repetierMqttClient.TopicActions.TryGetValue(command, out var topic))
-                    {
-                        // map for serialization? include printer? placeholder for printer?
-                        await _repetierMqttClient.MqttClient.PublishAsync(topic, Encoding.UTF8.GetBytes(""));
-                    }
-                };
-
-                _repetierMqttClient.RepetierConnection.OnEvent += async (eventName, printer, eventData) =>
-                {
-                    // TODO: check mapping and publish eventData
-                    if(_repetierMqttClient.TopicsForEvents.TryGetValue(eventName, out var topic))
-                    {
-                        // map for serialization? include printer? placeholder for printer?
-                        await _repetierMqttClient.MqttClient.PublishAsync(topic, Encoding.UTF8.GetBytes(""));
-                    }
-                };
-
-                _repetierMqttClient.MqttClient.UseApplicationMessageReceivedHandler(async e =>
-                {
-                    
-                });
-
-                return _repetierMqttClient;
-            }
-
-
-
-        }
-
-        private List<MqttTopicFilter> Subscriptions { get; set; } = new List<MqttTopicFilter>();
-
-        private List<string> Topics { get; set; } = new List<string>();
-
-        private Dictionary<string, string> TopicActions { get; set; } = new Dictionary<string, string>();
-
-        // Topic -> Command to execute
-        private Dictionary<string, ICommandData> CommandMapping { get; set; } = new Dictionary<string, ICommandData>();
+        /// <summary>
+        /// Mapping for events -> topics
+        /// Used to determine where to publish the data of an event
+        /// </summary>
+        private Dictionary<string, MqttTopicFilter> EventTopics { get; set; } = new Dictionary<string, MqttTopicFilter>();
 
         /// <summary>
-        /// TODO: add QOS?
-        /// Event -> Topic
+        /// Mapping for command -> topics
+        /// Used to determine where to publish the response data of a sent command
         /// </summary>
-        private Dictionary<string, string> TopicsForEvents { get; set; } = new Dictionary<string, string>();
+        private Dictionary<string, MqttTopicFilter> CommandResponseTopics { get; set; } = new Dictionary<string, MqttTopicFilter>();
 
-        private RepetierConnection RepetierConnection { get; set; }
+        public RepetierConnection RepetierConnection { get; private set; }
 
-        public string BaseTopic { get; private set; }
+        public string BaseTopic { get; set; }
 
         private IMqttClient MqttClient { get; set; }
 
@@ -198,9 +37,29 @@ namespace RepetierSharp.RepetierMqtt
 
         private uint ReconnectDelay { get; set; } = 3000;
 
-        private MqttQualityOfServiceLevel DefaultQoS { get; set; } = 0;
+        // TODO:
+        /// <summary>
+        /// Topic -> Command to execute 
+        /// </summary>
+        private Dictionary<string, ICommandData> CommandMapping { get; set; } = new Dictionary<string, ICommandData>();
 
-        public RepetierMqttClient() { }
+        /// <summary>
+        /// Topic for starting / uploading gcode.
+        /// Expected payload: UploadGCodeCommand
+        /// </summary>
+        public MqttTopicFilter UploadGCodeTopic { get; private set; }
+
+        /// <summary>
+        /// Topic for executing repetier commands.
+        /// Expected payload depending on command.
+        /// </summary>
+        public MqttTopicFilter ExecuteCommandTopic { get; private set; }
+
+        public MqttTopicFilter DefaultResponseTopic { get; private set; }
+
+        public MqttTopicFilter DefaultEventTopic { get; private set; }
+  
+        private RepetierMqttClient() { }
 
         public Task<MqttClientConnectResult> Connect()
         {
--- RepetierMqttClientBuilder.cs
+++ RepetierMqttClientBuilder.cs
@@ -1,10 +1,264 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Subscribing;
+using MQTTnet.Protocol;
+using RepetierMqtt.Util;
 using RepetierSharp;
 using RepetierSharp.RepetierMqtt;
+using RepetierSharp.RepetierMqtt.Util;
 
-namespace RepetierMqtt
+namespace RepetierSharp.RepetierMqtt
 {
-   
+    public partial class RepetierMqttClient
+    {
+
+        public class RepetierMqttClientBuilder
+        {
+
+            private RepetierMqttClient _repetierMqttClient = new RepetierMqttClient();
+
+            public RepetierMqttClientBuilder()
+            {
+
+            }
+
+            public RepetierMqttClientBuilder WithRepetierConnection(RepetierConnection repetierConnection)
+            {
+                _repetierMqttClient.RepetierConnection = repetierConnection;
+                return this;
+            }
+
+            public RepetierMqttClientBuilder WithMqttClientOptions(IMqttClientOptions options)
+            {
+                _repetierMqttClient.MqttClientOptions = options;
+                return this;
+            }
+
+            public RepetierMqttClientBuilder WithBaseTopic(string baseTopic)
+            {
+                _repetierMqttClient.BaseTopic = baseTopic;
+                return this;
+            }
+
+            // TODO: how to integrade printer and event info
+            public RepetierMqttClientBuilder WithDefaultEventTopic(string topic, int qos = 0, bool retained = false)
+            {
+                _repetierMqttClient.DefaultEventTopic = BuildTopicFilter(topic, qos, retained);
+                return this;
+            }
+
+            // TODO: how to integrade printer and command info
+            public RepetierMqttClientBuilder WithDefaultResponseTopic(string topic, int qos = 0, bool retained = false)
+            {
+                _repetierMqttClient.DefaultResponseTopic = BuildTopicFilter(topic, qos, retained);
+                return this;
+            }
+
+            // TODO: how to integrade printer info
+            public RepetierMqttClientBuilder WithEventTopic(string eventName, string topic, int qos = 0, bool retained = false)
+            {
+                _repetierMqttClient.EventTopics.Add(eventName, BuildTopicFilter(topic, qos, retained));
+                return this;
+            }
+
+            // TODO: how to integrade printer info
+            public RepetierMqttClientBuilder WithCommandResponseTopic(string command, string topic, int qos = 0, bool retained = false)
+            {
+                _repetierMqttClient.CommandResponseTopics.Add(command, BuildTopicFilter(topic, qos, retained));
+                return this;
+            }
+
+            public RepetierMqttClientBuilder WithReconnectDelay(uint delayInMs = 3000)
+            {
+                _repetierMqttClient.ReconnectDelay = delayInMs;
+                return this;
+            }
+
+            public RepetierMqttClientBuilder WithUploadGCodeTopic(string topic, int qos = 0, bool retained = false)
+            {
+                _repetierMqttClient.UploadGCodeTopic = BuildTopicFilter(topic, qos, retained);
+                return this;
+            }
+
+            public RepetierMqttClientBuilder WithExecuteCommandTopic(string topic, int qos = 0, bool retained = false)
+            {
+                _repetierMqttClient.ExecuteCommandTopic = BuildTopicFilter(topic, qos, retained);
+                return this;
+            }
+
+            public RepetierMqttClient Build()
+            {
+                if (_repetierMqttClient.RepetierConnection == null)
+                {
+                    throw new ArgumentNullException("RepetierConnection needs to be provided");
+                }
+
+                if (_repetierMqttClient.MqttClientOptions == null)
+                {
+                    _repetierMqttClient.MqttClientOptions = MqttOptionsProvider.DefaultMqttClientOptions;
+                }
+
+                _repetierMqttClient.MqttClient = new MqttFactory().CreateMqttClient();
+
+                _repetierMqttClient.MqttClient.UseConnectedHandler(async connectedArgs =>
+                {
+                    if (_repetierMqttClient.DefaultEventTopic != null)
+                    {
+                        await _repetierMqttClient.MqttClient.SubscribeAsync(_repetierMqttClient.DefaultEventTopic);
+                    }
+
+                    if (_repetierMqttClient.DefaultResponseTopic != null)
+                    {
+                        await _repetierMqttClient.MqttClient.SubscribeAsync(_repetierMqttClient.DefaultResponseTopic);
+                    }
+
+                    if (_repetierMqttClient.ExecuteCommandTopic != null)
+                    {
+                        await _repetierMqttClient.MqttClient.SubscribeAsync(_repetierMqttClient.ExecuteCommandTopic);
+                    }
+
+                    if (_repetierMqttClient.UploadGCodeTopic != null)
+                    {
+                        await _repetierMqttClient.MqttClient.SubscribeAsync(_repetierMqttClient.UploadGCodeTopic);
+                    }
+
+                    _repetierMqttClient.RepetierConnection.OnRawEvent += async (eventName, printer, eventData) =>
+                    {
+                        if (_repetierMqttClient.DefaultEventTopic != null)
+                        {
+                            var eventTopic = "";
+                            if (!string.IsNullOrEmpty(printer))
+                            {
+                                eventTopic = $"{_repetierMqttClient.DefaultEventTopic.Topic}/{printer}/{eventName}";
+                            }
+                            else
+                            {
+                                eventTopic = $"{_repetierMqttClient.DefaultEventTopic.Topic}/{eventName}";
+                            }
+                            var topicFilter = new MqttTopicFilterBuilder()
+                                .WithQualityOfServiceLevel(_repetierMqttClient.DefaultEventTopic.QualityOfServiceLevel)
+                                .WithTopic(eventTopic)
+                                .WithRetainAsPublished(_repetierMqttClient.DefaultEventTopic.RetainAsPublished)
+                                .Build();
+                            await _repetierMqttClient.MqttClient.PublishAsync(BuildMessage(topicFilter, eventData));
+                        }
+                        if (_repetierMqttClient.EventTopics.TryGetValue(eventName, out var topic))
+                        {
+                            await _repetierMqttClient.MqttClient.PublishAsync(topic.Topic, eventData);
+                        }
+                    };
+
+                    _repetierMqttClient.RepetierConnection.OnRawResponse += async (callbackID, command, response) =>
+                    {
+                        if (_repetierMqttClient.DefaultResponseTopic != null)
+                        {
+                            var topicFilter = new MqttTopicFilterBuilder()
+                                .WithQualityOfServiceLevel(_repetierMqttClient.DefaultResponseTopic.QualityOfServiceLevel)
+                                .WithTopic($"{_repetierMqttClient.DefaultResponseTopic.Topic}/{command}")
+                                .WithRetainAsPublished(_repetierMqttClient.DefaultResponseTopic.RetainAsPublished)
+                                .Build();
+                            await _repetierMqttClient.MqttClient.PublishAsync(BuildMessage(topicFilter, response));
+                        }
+                        if (_repetierMqttClient.CommandResponseTopics.TryGetValue(command, out var topic))
+                        {
+                            await _repetierMqttClient.MqttClient.PublishAsync(BuildMessage(topic, response));
+                        }
+                    };
+                });
+
+                _repetierMqttClient.MqttClient.UseDisconnectedHandler(async disconnectedArgs =>
+                {
+                    await Task.Delay(TimeSpan.FromMilliseconds(_repetierMqttClient.ReconnectDelay));
+                    try
+                    {
+                        await _repetierMqttClient.Connect();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.Error.WriteLine(e);
+                    }
+                });
+
+
+                _repetierMqttClient.MqttClient.UseApplicationMessageReceivedHandler(e =>
+                {
+                    Console.WriteLine("Received");
+                    var topic = e.ApplicationMessage.Topic;
+                    if (_repetierMqttClient.ExecuteCommandTopic != null && topic == _repetierMqttClient.ExecuteCommandTopic.Topic)
+                    {
+                        try
+                        {
+                            // _repetierMqttClient.RepetierConnection.SendCommand(e.ApplicationMessage.Payload);
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Error.WriteLine($"{ex.Message}");
+                        }
+                    }
+
+                    if (_repetierMqttClient.UploadGCodeTopic != null && topic == _repetierMqttClient.UploadGCodeTopic.Topic)
+                    {
+                        try
+                        {
+                            // TODO: validate payload
+                            var gcodeCommand = JsonSerializer.Deserialize<UploadGCodeCommand>(e.ApplicationMessage.Payload);
+                            if (gcodeCommand.Autostart)
+                            {
+                                _repetierMqttClient.RepetierConnection.UploadAndStartPrint(gcodeCommand.FilePath, gcodeCommand.Printer);
+                            }
+                            else
+                            {
+                                _repetierMqttClient.RepetierConnection.UploadGCode(gcodeCommand.FilePath, gcodeCommand.Group, gcodeCommand.Printer, gcodeCommand.Overwrite);
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.Error.WriteLine($"{ex.Message}");
+                        }
+                    }
+
+                });
+
+                return _repetierMqttClient;
+            }
+
+            private MqttApplicationMessage BuildMessage(MqttTopicFilter topic, byte[] payload)
+            {
+                return new MqttApplicationMessageBuilder()
+                    .WithTopic(topic.Topic)
+                    .WithQualityOfServiceLevel(topic.QualityOfServiceLevel)
+                    .WithRetainFlag(topic.RetainAsPublished)
+                    .WithPayload(payload)
+                    .Build();
+            }
+
+            private MqttTopicFilter BuildTopicFilter(string topic, int qos = 0, bool retained = false)
+            {
+                return new MqttTopicFilterBuilder()
+                    .WithQualityOfServiceLevel((MqttQualityOfServiceLevel)qos)
+                    .WithTopic(BuildTopic(topic))
+                    .WithRetainAsPublished(retained)
+                    .Build();
+            }
+
+            private string BuildTopic(string topic)
+            {
+                if (string.IsNullOrEmpty(_repetierMqttClient.BaseTopic))
+                {
+                    return topic;
+                }
+                else
+                {
+                    return $"{_repetierMqttClient.BaseTopic}/{topic}";
+                }
+            }
+        }
+    }
 }
