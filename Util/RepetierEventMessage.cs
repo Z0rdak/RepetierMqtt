555e484a87cd233b1b09c94131b8efb87a7f936c
Marcel Bartholet <Marcel.Bartholet@mni.thm.de>
2022-02-04 07:51:09 +0100

    introduce types for event and response payload

---
+++ Util/RepetierEventMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace RepetierSharp.RepetierMqtt.Util
+{
+    public class RepetierEventMessage
+    {
+        [JsonPropertyName("event")]
+        public string EventName { get; set; }
+
+        [JsonPropertyName("printer")]
+        public string Printer { get; set; }
+
+        [JsonPropertyName("data")]
+        public Dictionary<string, object> Data { get; set; }
+    }
+}
---
+++ Util/RepetierResponseMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RepetierSharp.RepetierMqtt.Util
+{
+    public class RepetierResponseMessage
+    {
+
+        [JsonPropertyName("callbackId")]
+        public int CallbackId { get; set; }
+
+        [JsonPropertyName("command")]
+        public string Command { get; set; }
+
+        [JsonPropertyName("data")]
+        public Dictionary<string, object> Data { get; set; }
+    }
+}
